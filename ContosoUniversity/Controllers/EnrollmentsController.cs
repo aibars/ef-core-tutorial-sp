974cf2452dd8438dc55131aa3b3764921507fa88
Agustín Ibars <agustin.ibars@gmail.com>
2018-02-06 23:57:25 -0300

    Add simple sp

---
+++ ContosoUniversity/Controllers/EnrollmentsController.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Enrollments")]
+    public class EnrollmentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public EnrollmentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Student>> Get()
+        {
+            return await _context.Students.FromSql("[GetEnrollmentsByStudent]").ToListAsync();
+        }
+    }
+}
---
+++ ContosoUniversity/Data/spEnrollmentsByStudents.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Data
+{
+    public class spEnrollmentsByStudents : Migration
+    {
+        protected override void Up( MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+                CREATE PROCEDURE [GetEnrollmentsByStudent]
+                AS
+                BEGIN
+                SELECT * FROM Students a INNER JOIN Enrollments
+                b ON b.StudentId = a.Id
+                END";
+
+            migrationBuilder.Sql(sp);
+        }
+    }
+}
